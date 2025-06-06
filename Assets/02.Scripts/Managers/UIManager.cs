c5eea82cc7d61b5ef8c863d6ff39bccbb4d7530e
KGH1125 <128346998+KGH1125@users.noreply.github.com>
2025-06-07 02:40:43 +0900

    feat : UI디자인 제작

--- Assets/02.Scripts/Managers/UIManager.cs
+++ Assets/02.Scripts/Managers/UIManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UIManager Instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    UIMainMenu uIMainMenu;
+    [SerializeField]
+    UIStatus uIStatus;
+    [SerializeField]
+    UIInventory uIInventory;
+
 }
