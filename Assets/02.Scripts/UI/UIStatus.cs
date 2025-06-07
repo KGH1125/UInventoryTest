5c2fe201a5455a9d6849ab14e466cc0c2eaadac9
KGH1125 <128346998+KGH1125@users.noreply.github.com>
2025-06-07 09:53:28 +0900

    feat : 플레이어 스텟창 갱신추가

--- Assets/02.Scripts/UI/UIStatus.cs
+++ Assets/02.Scripts/UI/UIStatus.cs
@@ -1,8 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIStatus : MonoBehaviour
 {
-    
+    [SerializeField] private TextMeshProUGUI AttackData;
+    [SerializeField] private TextMeshProUGUI DefenseData;
+    [SerializeField] private TextMeshProUGUI HealthData;
+    [SerializeField] private TextMeshProUGUI CriticalData;
+
+    private Character player;
+
+    private void Start()
+    {
+        player = GameManager.instance.Player;
+        UpdateUIStatus();
+    }
+
+    public void UpdateUIStatus()
+    {
+        AttackData.text = $"{player.Attack}";
+        DefenseData.text= $"{player.Defense}";
+        HealthData.text= $"{player.Health}";
+        CriticalData.text= $"{player.Critical}";
+    }
 }
